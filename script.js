2abdad88f108432a3df4802e202080c5e7e4cf4c
ikalumba <ikalumba221@gmail.com>
2023-11-01 17:48:55 +0300

    Implementing functionality

--- script.js
+++ script.js
@@ -103,3 +103,71 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
 });
 */
+
+///////////////////////////////////
+// REDO
+
+let score = 20;
+let highScore = 0;
+
+let magicNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(magicNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  // let message = document.querySelector('.message').textContent;
+  const guessNum = Number(document.querySelector('.guess').value);
+  console.log(guessNum);
+
+  // When someone wins
+  if (guessNum === magicNumber) {
+    document.querySelector('.message').textContent =
+      'Congratulations!! You win🎉';
+    document.querySelector('.number').textContent = magicNumber;
+    document.querySelector('body').style.backgroundColor = 'yellow';
+    if (score > highScore) {
+      highScore = score;
+
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('.check').disabled = true;
+
+    // When the guess is higher than the magic number
+  } else if (guessNum > magicNumber) {
+    document.querySelector('.message').textContent = '😱 Too high';
+    score -= 1;
+
+    // When the guess is less than the magic number
+  } else if (guessNum < magicNumber && guessNum > 0) {
+    document.querySelector('.message').textContent = '😥 Too low';
+    score -= 1;
+  }
+
+  //  When no number is input
+  // if()
+
+  if (!guessNum) {
+    document.querySelector('.message').textContent = '🙅🏾‍♂️ No number!';
+    score = score;
+  }
+
+  if (score === 0) return;
+
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  magicNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  console.log(magicNumber);
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#003559';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.check').disabled = false;
+});
